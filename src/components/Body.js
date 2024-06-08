f215a35f1b876a5a89bae047b3ccedcecd5e52ac
ket8n <ketanchopade.kc@gmail.com>
2024-06-08 19:18:00 +0530

    Fetching From Live API. Added Shimmer.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import resListData from "../utils/mockData";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [resList, setResList] = useState(resListData);
+  const [resList, setResList] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2156354&lng=72.63694149999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    setResList(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (resList.length === 0) {
+    return <Shimmer />;
+  }
 
   return (
     <div className="body">
@@ -21,7 +41,7 @@ const Body = () => {
       </button>
       <div className="res-container">
         {resList.map((restaurant) => (
-          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+          <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
         ))}
       </div>
     </div>
