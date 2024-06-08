a7a3a66c52e261b6902243169cd8348f6e0c7343
ket8n <ketanchopade.kc@gmail.com>
2024-06-08 18:37:31 +0530

    Before Live API [MockData in action]

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
+import resListData from "../utils/mockData";
 
-const Body = () => (
-  <div className="body">
-    <div className="search">Search</div>
-    <div className="res-container">
-      {resList.map((restaurant) => (
-        <RestaurantCard key={restaurant.info.id} resData={restaurant} />
-      ))}
+const Body = () => {
+  const [resList, setResList] = useState(resListData);
+
+  return (
+    <div className="body">
+      <button
+        className="filter-btn"
+        onClick={() => {
+          const filteredResList = resList.filter(
+            (res) => res.info.avgRating > 4.4
+          );
+
+          setResList(filteredResList);
+        }}
+      >
+        Top Rated Restaurant
+      </button>
+      <div className="res-container">
+        {resList.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Body;
